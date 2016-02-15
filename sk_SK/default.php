a219d76608b93ba9a1f4737c41b7f9ad5188835a
petermiksik <peter@miksik.sk>
2016-02-15 21:14:21 +0100

    Update default.php

--- sk_SK/default.php
+++ sk_SK/default.php
@@ -2,10 +2,10 @@
 
 $strings = array(
     // Date format including time (see http://php.net/manual/en/function.date.php for possible values)
-    "_DATE_FORMAT_LONG" => "j.n.Y G:i",
+    "_DATE_FORMAT_LONG" => "j. n. Y G:i",
 
     // Date format without time (see http://php.net/manual/en/function.date.php for possible values)
-    "_DATE_FORMAT" => "j.n.Y",
+    "_DATE_FORMAT" => "j. n. Y",
 
     // Name of system account
     "System" => "Systém",
