ef1e3ed45ba7fa490585acd993974b4164ab3e92
petermiksik <peter@miksik.sk>
2016-02-15 21:11:23 +0100

    Update cookies.php

--- sk_SK/cookies.php
+++ sk_SK/cookies.php
@@ -4,7 +4,7 @@
 
 $strings = array(
     "This website uses cookies to ensure you get the best experience on our website." =>
-        "Tento web používá k poskytování služeb a analýze návštěvnosti soubory cookie. Používáním tohoto webu s tím souhlasíte.",
-    "Agree" => "V pořádku",
-    "More information" => "Další informace",
+        "Tento web používa na poskytovanie služieb a analýzu návštevnosti súbory cookie. Používaním tohto webu s tým súhlasíte.",
+    "Agree" => "V poriadku",
+    "More information" => "Ďalšie informácie",
 );
