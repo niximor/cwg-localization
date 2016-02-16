db9bf61ecae37277da51ac259dd68cecdf4d4daf
petermiksik <peter@miksik.sk>
2016-02-16 15:43:19 +0100

    Update forms.php

--- sk_SK/forms.php
+++ sk_SK/forms.php
@@ -1,7 +1,5 @@
 <?php
-
 // Various form messages.
-
 $strings = array(
     "Field '%s' must be filled in." => "Položka '%s' musí byť vyplnená.",
     "%s must have at least %d chars." => "%s musí mať aspoň %d znakov.",
@@ -10,14 +8,14 @@ $strings = array(
     "Cancel" => "Storno",
     "You must select at least one item in '%s'" => "Musíte vybrať aspoň jednu položku v '%s'",
     "Field '%s' contains invalid email address." => "Položka '%s' obsahuje neplatnú e-mailovú adresu.",
-    "Field '%s̈́' contains invalid value." ="Položka '%s' obsahujechybnúu hodnotu.",
-    "You are required to check the checkbox '%s'." => "Musíte zaškrtnout '%s'.",
-    "This form has already been submitted." => "Tento formulář byl již odeslán.",
+    "Field '%s̈́' contains invalid value." ="Položka '%s' obsahuje chybnú hodnotu.",
+    "You are required to check the checkbox '%s'." => "Musíte začiarknuť '%s'.",
+    "This form has already been submitted." => "Tento formulár bol už odoslaný.",
     "(step %d of %d)" => "(krok %d z %d)",
-    "&lt; Previous" => "&lt; Předchozí",
-    "Next &gt;" => "Následující &gt;",
+    "&lt; Previous" => "&lt; Predch.",
+    "Next &gt;" => "Nasled. &gt;",
     "Number out of range in '%s'." => "Číslo v '%s' je mimo povolený rozsah.",
-    "Up" => "Nahoru",
-    "Down" => "Dolů",
-    "Add another file" => "Přidat další soubor",
+    "Up" => "Nahor",
+    "Down" => "Nadol",
+    "Add another file" => "Pridať ďalší súbor",
 );
