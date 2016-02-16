b99cdd4029a5e1527f666a78e8ab1c115526f67d
petermiksik <peter@miksik.sk>
2016-02-16 16:00:01 +0100

    Update cwg_ops.php

--- sk_SK/cwg_ops.php
+++ sk_SK/cwg_ops.php
@@ -1,63 +1,51 @@
 <?php
-
 // New xWG form and related messages
-
 $strings = array(
-    "Insert new xWG" => "Vložit nové xWG",
+    "Insert new xWG" => "Vložiť nové xWG",
     "This page can be used to enter xWG, that isn't (so far) present in the official catalogue. Please, take extreme caution of what you are entering. Mainly, please, pay attention to <strong>xWG NAME</strong>, which should contain exactly what is written <strong>AROUND THE xWG</strong>, not inside. Before you submit new xWG, please check, that xWG is not entered with some similar name (for example including \"&amp; team\", \"&amp; family\" and similar). You will spare administrator's time who will need to manually fix your error when it is found. When you enter incorrect xWG multiple times, it may lead to revocation of your privilege to insert new items." =>
-        "Slouží k zadání xWG, které (prozatím) není umístěno v katalogu. Při zadávání si dejte pozor, co zadáváte. Zejména <strong>NÁZEV xWG</strong>, který by měl přesně odpovídat tomu, co je napsáno <strong>PO OBVODU xWG</strong>, nikoliv uvnitř. Před zadáním také, prosím, zkontrolujte, není-li již xWG zadáno s nějakým podobným názvem (např. \"&amp; team\", \"&amp; family\" apod.). Ušetříte tak práci i nervy administrátorům v době, kdy někdo na chybu přijde administrátoři ji pak budou muset ručně řešit. Opakované chybné zadávání xWG může být potrestáno až zákazem vkládání dalších záznamů.",
-
+        "Slúži na zadanie xWG, ktoré (zatiaľ) nie je umiestnené v katalógu. Pri zadávaní si dajte pozor, čo zadávate. Najmä <strong>NÁZOV xWG</strong>, ktorý by mal presne zodpovedať tomu, čo je napísané <strong>PO OBVODE xWG</strong>, nie vo vnútri. Pred zadaním tiež skontrolujte, či už nie je xWG zadané s nejakým podobným názvom (napr. \"&amp; team\", \"&amp; family\" apod.). Ušetríte tak prácu aj nervy administrátorom v dobe, kedy niekto na chybu príde, a administrátori ju potom budú musieť ručne riešiť. Opakované chybné zadávanie xWG môže byť potrestané až zákazom vkladania ďalších záznamov.",
     "Describe the picture on the xWG into the <strong>Note</strong> field. This field is required." =>
-        "Do pole <strong>Poznámka</strong> napište, co je na xWG za motiv, případně nějaký jiný způsob, jak toto CWG identifikovat. Toto pole je nyní povinné.",
-
+        "Do položky <strong>Poznámka</strong> napíšte, aký je na xWG motív, prípadne nejaký iný spôsob, ako toto xWG identifikovať. Táto položka je teraz povinná.",
     "xWG information" => "Údaje o xWG",
     "Enter what is written <strong>AROUND</strong> xWG." =>
-        "Vepište to, co je <strong>PO OBVODU</strong> xWG.",
+        "Vpíšte to, čo je <strong>PO OBVODE</strong> xWG.",
     "Write by words, what image is burned on the xWG." =>
-        "Popište slovy obrázkový motiv, který je na xWG vypálený.",
-
-    "Insert" => "Vložit",
-
+        "Popíšte slovami obrázkový motív, ktorý je na xWG vypálený.",
+    "Insert" => "Vložiť",
     "Another xWG with same name and version already exists in this catalogue." =>
-        "xWG s tímto názvem a verzí v katalogu již existuje.",
+        "xWG s týmto názvom a verziou v katalógu už existuje.",
     "xWG has been successfully placed into the catalogue." =>
-        "xWG bylo úspěšně vloženo do katalogu.",
-
-    "xWGs entered by me" => "Mnou zadaná xWG",
-    "Users<sup>*</sup>" => "Uživatelů<sup>*</sup>",
-
-    "Modify" => "Upravit",
-    "Modify xWG %s (version %s)" => "Upravit xWG %s (verze %s)",
-    "Modify xWG %s" => "Upravit xWG %s",
-    "xWG has been successfully modified." => "xWG bylo úspěšně upraveno.",
-    "You have insufficient privileges." => "Nemáte dostatečná oprávnění.",
-
-    "Remove" => "Odstranit",
-    "Do you really want to remove this xWG? Operation cannot be undone." => "Skutečně chcete toto xWG smazat? Operace je nevratná!",
-    "xWG has been successfully removed." => "xWG bylo úspěšně odstraněno.",
+        "xWG bolo úspešne vložené do katalógu.",
+    "xWGs entered by me" => "Mnou zadané xWG",
+    "Users<sup>*</sup>" => "Používateľov<sup>*</sup>",
+    "Modify" => "Upraviť",
+    "Modify xWG %s (version %s)" => "Upraviť xWG %s (verzia %s)",
+    "Modify xWG %s" => "Upraviť xWG %s",
+    "xWG has been successfully modified." => "xWG bolo úspešne upravené.",
+    "You have insufficient privileges." => "Nemáte dostatočné oprávnenia.",
+    "Remove" => "Odstrániť",
+    "Do you really want to remove this xWG? Operation cannot be undone." => "Naozaj chcete toto xWG odstrániť? Úkon sa nedá vrátiť!",
+    "xWG has been successfully removed." => "xWG bolo úspešne odstránené.",
     "xWG cannot be removed, because it is collected by at least one user." =>
-        "xWG nejde smazat, pokud ho má v kolekci alespoň jeden uživatel.",
-
-    "Join to..." => "Sloučit s...",
-    "Join xWG %s (version %s)" => "Sloučit xWG %s (verze %s)",
-    "Join xWG %s" => "Sloučit xWG %s",
+        "xWG nie je možné odstrániť, ak ho má v zbierke aspoň jeden používateľ.",
+    "Join to..." => "Zlúčiť s...",
+    "Join xWG %s (version %s)" => "Zlúčiť xWG %s (verzia %s)",
+    "Join xWG %s" => "Zlúčiť xWG %s",
     "You cannot join official xWG to unofficial. Shouldn't it be the other way?" =>
-        "Nelze sloučit oficiální xWG do neoficiálního. Nemělo by to být naopak?",
-    "You cannot join xWG to itself." => "Nelze sloučit xWG samo do sebe.",
-    "xWG %s has been joined with %s." => "xWG %s bylo sloučeno s %s.",
-    "Information about xWG join" => "Informace o sloučení xWG",
+        "Nedá sa zlúčiť oficiálne xWG do neoficiálneho. Nemalo by to byť naopak?",
+    "You cannot join xWG to itself." => "Nedá sa zlúčiť xWG samo do seba.",
+    "xWG %s has been joined with %s." => "xWG %s bolo zlúčené s %s.",
+    "Information about xWG join" => "Informácie o zlúčení xWG",
     "xWG&nbsp;%s, which you offer for exchange, has been joined to xWG&nbsp;%s. Please, check your offer whether it is still valid." =>
-        "xWG&nbsp;%s, které nabízíte k výměně, bylo sloučeno s xWG&nbsp;%s. Zkontrolujte prosím Vaši nabídku, zdali je v pořádku a stále platná.",
+        "xWG&nbsp;%s, ktoré ponúkate na výmenu, bolo zlúčené s xWG&nbsp;%s. Skontrolujte svoju ponuku, či je v poriadku a stále platná.",
     "xWG&nbsp;%s, which you have in your collection, has been joined to xWG&nbsp;%s. Please, check that this is correct and the xWG is really that one you have in your collection." =>
-        "xWG&nbsp;%s, které máte ve vaší kolekci, bylo sloučeno s xWG&nbsp;%s. Zkontrolujte prosím, že je vše v pořádku a toto nové CWG je skutečně to, které máte ve sbírce.",
+        "xWG&nbsp;%s, ktoré máte vo svojej zbierke, bolo zlúčené s xWG&nbsp;%s. Skontrolujte, či je všetko v poriadku a toto nové xWG je skutočne to, ktoré máte v zbierke.",
     "xWG&nbsp;%s, which you want, has been joined to xWG&nbsp;%s. Please, check whether you still want this piece." =>
-        "xWG&nbsp;%s, které sháníte, bylo sloučeno s xWG&nbsp;%s. Prosím zkontrolujte, že se stále jedná o kousek, který sháníte.",
+        "xWG&nbsp;%s, ktoré zháňate, bolo zlúčené s xWG&nbsp;%s. Skontrolujte, či ide stále o kúsok, ktorý zháňate.",
     "xWG&nbsp;%s, which you don't want, has been joined to xWG&nbsp;%s. Please, check whether this is the piece that you don't want." =>
-        "xWG&nbsp;%s, které nechcete, bylo sloučeno s xWG&nbsp;%s. Prosím, zkontrolujte, že se stále jedná o kousek, který nechcete.",
-    "Join has been performed by %s." => "Sloučení provedl %s.",
-
-    "Picture" => "Obrázek",
-
+        "xWG&nbsp;%s, ktoré nechcete, bolo zlúčené s xWG&nbsp;%s. Skontrolujte, či ide stále o kúsok, ktorý nechcete.",
+    "Join has been performed by %s." => "Zlúčenie vykonal/a %s.",
+    "Picture" => "Obrázok",
     "* Number of users, which has this xWG in their collection. xWG can be removed only when noone has it in the collection." =>
-        "* Počet uživatelů, kteří mají toto xWG ve své kolekci. xWG jde smazat pouze tehdy, pokud ho nikdo nemá v kolekci.",
+        "* Počet používateľov, ktorí majú toto xWG vo svojej zbierke. xWG sa dá odstrániť len vtedy, ak ho nikto nemá v zbierke.",
 );
