5dc7b5d90552b9c6fae1126586d6fe20a2ad1eb4
petermiksik <peter@miksik.sk>
2016-02-16 15:39:12 +0100

    Update api.php

--- sk_SK/api.php
+++ sk_SK/api.php
@@ -5,6 +5,6 @@
 $strings = array(
     "API" => "API",
     "API (application interface) is used to communicate with CWG Collection from external applications. It's usage is free and is not limited. However, access to API can be limited when excessive ussage is detected." =>
-        "API (aplikačné rozhranie) slúži na komunikáciu s CWG kolekciou z externých aplikácií. Jeho použitie je zadarmo a nie je obmedzené, ale prevádzkovateľ si vyhradzuje právo obmedziť prístup k niektorým funkciám v prípade nadmerného zaťažovania systému príliš veľkým množstvom dopytov.",
+        "API (aplikačné rozhranie) slúži na komunikáciu s aplikáciou CWG Kolekcia z externých aplikácií. Jeho použitie je zadarmo a nie je obmedzené, ale prevádzkovateľ si vyhradzuje právo obmedziť prístup k niektorým funkciám v prípade nadmerného zaťažovania systému príliš veľkým množstvom dopytov.",
     "API documentation" => "Dokumentácia k API",
 );
