6e9f2dee4ca7523f7110fd25db573ef03b8f5cf2
petermiksik <peter@miksik.sk>
2016-02-16 16:13:46 +0100

    Update export.php

--- sk_SK/export.php
+++ sk_SK/export.php
@@ -1,60 +1,48 @@
 <?php
-
 // Collection export
-
 $strings = array(
-    "Collection export" => "Export kolekce",
+    "Collection export" => "Export zbierky",
     "Export" => "Export",
-    "Existing exports" => "Existující exporty",
-    "Date" => "Datum",
+    "Existing exports" => "Existujúce exporty",
+    "Date" => "Dátum",
     "Format" => "Formát",
     "Pieces" => "Ks",
-    "Remaining" => "Zbývá",
-    "Export!" => "Exportovat",
-
+    "Remaining" => "Zostáva",
+    "Export!" => "Exportovať",
     "Export has been queued for processing. As soon at is is done, it will be available for downloading at the export page." =>
-        "Export byl zařazen do fronty ke zpracování. Jakmile bude hotový, bude k dispozici ke stažení na stránce s exporty.",
+        "Export bol zaradený do frontu na spracovanie. Keď bude hotový, bude k dispozícii na stiahnutie na stránke s exportmi.",
     "Unable to retrieve exported file." =>
-        "Nepodařilo se stáhnout soubor s exportem.",
-    "Export has been removed." => "Export byl odstraněn.",
-
+        "Nepodarilo sa stiahnuť súbor s exportom.",
+    "Export has been removed." => "Export bol odstránený.",
     "%d days" => array(
         0 => "%d dní",
-        1 => "%d den",
-        2 => "%d dny",
-        3 => "%d dny",
-        4 => "%d dny",
-        5 => "%d dnů",
+        1 => "%d deň",
+        2 => "%d dni",
+        3 => "%d dni",
+        4 => "%d dni",
+        5 => "%d dní",
     ),
-
-    "Download" => "Stáhnout",
-    "Remove" => "Odstranit",
-
-    "Error while processing the export." => "Chyba při zpracování.",
-    "Processing..." => "Zpracovává se...",
-    "Waiting to be processed." => "Čeká na spuštění.",
-
+    "Download" => "Stiahnuť",
+    "Remove" => "Odstrániť",
+    "Error while processing the export." => "Chyba pri spracovaní.",
+    "Processing..." => "Spracováva sa...",
+    "Waiting to be processed." => "Čaká na spustenie.",
     "You don't have any export. To create an export, select export format and click the Export button." =>
-        "Nemáte vytvořen žádný export. Pro jeho vytvoření si vyberte formát, v jakém chcete sbírku vyexportovat a klikněte na tlačítko Exportovat.",
-
+        "Nemáte vytvorený žiadny export. Pre jeho vytvorenie si vyberte formát, v akom chcete zbierku vyexportovať a kliknite na tlačidlo Exportovať.",
     "New export" => "Nový export",
     "You can use this function to export your collection in one of supported formats. Resulting file can be used on your gc.com profile or as input to other applications." =>
-        "Tuto funkci můžete použít k exportu vaší kolekce v jednom z podporovaných formátů. Výsledný soubor poté můžete použít třeba pro Váš profil na gc.com nebo jako vstup do dalších programů.",
+        "Túto funkciu môžete použiť na export vašej zbierky v jednom z podporovaných formátov. Výsledný súbor potom môžete použiť napríklad pre svoj profil na gc.com alebo ako vstup do ďalších programov.",
     "Supported formats" => "Podporované formáty",
-
     "HTML" => "HTML",
     "HTML page is suitable for usage in your geocaching.com profile. Export contains only xWGs, you have in your collection." =>
-        "HTML stránka vhodná pro použití například v profilu. Exportuje pouze xWG, která máte ve vaší kolekci.",
-
-    "Full XML" => "Kompletní XML",
+        "HTML stránka vhodná na použitie napríklad v profile. Exportuje len xWG, ktorá máte vo svojej zbierke.",
+    "Full XML" => "Kompletné XML",
     "Full XML contains everything you have entered in this system. It contains all xWGs which you have collected, you offers, you want or you don't want." =>
-        "Kompletní XML informace o celé vaší kolekci, XML obsahuje veškeré údaje, které jsou v systému dostupné. Exportuje všechna CWG, u kterých máte nastaven alespoň jeden z příznaků \"mám\", \"nabízím\", \"chci\", \"nechci\".",
-
+        "Kompletné XML informácie o celej vašej zbierke. XML obsahuje všetky údaje, ktoré sú v systéme dostupné. Exportuje všetky xWG, pri ktorých máte nastavený aspoň jeden z príznakov \"mám\", \"ponúkam\", \"chcem\", \"nechcem\".",
     "svetgc.cz XML" => "svetgc.cz XML",
     "XML compatible with export from former svetgc.cz catalogue. It contains all xWGs that you marked using one of \"collected\", \"offers\", \"wants\" and \"don't want\" attribute." =>
-        "XML kompatibilní s exportem ze svetgc.cz. Exportuje všechna CWG, u kterých máte nastaven alespoň jeden příznak \"mám\", \"nabízím\", \"chci\", \"nechci\".",
-
+        "XML kompatibilné s exportom zo svetgc.cz. Exportuje všetky xWG, pri ktorých máte nastavený aspoň jeden z príznakov \"mám\", \"ponúkam\", \"chcem\", \"nechcem\".",
     "GeoGet CSV" => "GeoGet CSV",
     "CSV file compatible with export from GeoGet CWG addon. Only xWGs you have in your collection are exported." =>
-        "CSV soubor kompatibilní s exportem z GeoGet CWG addonu. Exportuje pouze xWG, která máte ve vaší kolekci."
+        "CSV súbor kompatibilný s exportom z doplnku GeoGet CWG. Exportuje len xWG, ktoré máte vo svoje zbierke."
 );
