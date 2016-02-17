6fbd705f1c8a5f8d9fb4d5fbb743f2b95a1685c7
petermiksik <peter@miksik.sk>
2016-02-17 17:37:43 +0100

    Update layout.php

--- sk_SK/layout.php
+++ sk_SK/layout.php
@@ -1,86 +1,79 @@
 <?php
-
 // Page layout, common strings
-
 $strings = array(
     // meta description
     "Website for CWG collection catalogization. CWG catalogue." =>
-        "Webová služba pro katalogizaci sbírky CWG (Czech Wood Geocoin). CWG katalog.",
+        "Webová služba na katalogizáciu zbierky xWG (x Wood Geocoin). xWG katalóg.",
     "Introduce order to your (Czech) Wood Geocoin (xWG) collection using this website." =>
-        "Udělejte si pořádek ve své sbírce Czech Wood Geocoin (CWG) pomocí tohoto webu.",
-    "CWG collection catalogization" => "Katalogizace sbírky CWG",
-
+        "Urobte si poriadok vo svojej zbierke x Wood Geocoin (xWG) pomocou tohto webu.",
+    "CWG collection catalogization" => "Katalogizácia zbierky xWG",
     "<abbr title=\"Czech Wood Geocoin\">CWG</abbr> Collection" =>
-        "<abbr title=\"Czech Wood Geocoin\">CWG</abbr> Kolekce",
+        "<abbr title=\"Czech Wood Geocoin\">xWG</abbr> Zbierka",
     "Website for <abbr title=\"(Czech) Wood Geocoin\">xWG</abbr> collection catalogization." =>
-        "Webová služba pro katalogizaci sbírky <abbr title=\"Czech Wood Geocoin\">CWG</abbr>",
+        "Webová služba na katalogizáciu zbierky xWG",
     "You have %d unread messages" => array(
-        0 => "Máte %d nepřečtených zpráv.",
-        1 => "Máte %d nepřečtenou zprávu.",
-        2 => "Máte %d nepřečtené zprávy.",
-        3 => "Máte %d nepřečtené zprávy.",
-        4 => "Máte %d nepřečtené zprávy.",
-        5 => "Máte %d nepřečtených zpráv.",
+        0 => "Máte %d neprečítaných správ.",
+        1 => "Máte %d neprečítanú správu.",
+        2 => "Máte %d neprečítané správy.",
+        3 => "Máte %d neprečítané správy.",
+        4 => "Máte %d neprečítané správy.",
+        5 => "Máte %d neprečítaných správ.",
      ),
-
     // Main menu
-    "Login" => "Přihlásit",
-    "Overview" => "Přehled",
-    "Search" => "Vyhledávání",
-    "Browse catalogue" => "Procházet katalog",
-    "Catalogue" => "Katalog",
-    "My collection" => "Moje sbírka",
-    "Entry" => "Zadat",
-    "My account" => "Můj účet",
-    "Logout" => "Odhlásit",
-    "Best collectors" => "Nejlepší sběratelé",
-
+    "Login" => "Prihlásiť",
+    "Overview" => "Prehľad",
+    "Search" => "Vyhľadávanie",
+    "Browse catalogue" => "Listovať v katalógu",
+    "Catalogue" => "Katalóg",
+    "My collection" => "Moja zbierka",
+    "Entry" => "Zadať",
+    "My account" => "Môj účet",
+    "Logout" => "Odhlásiť",
+    "Best collectors" => "Najlepší zberatelia",
     // Login page
     "Do you collect <abbr title=\"Czech Wood Geocoin\">CWG</abbr>? Do you want to introduce order to your collection? Do you offer <abbr title=\"Czech Wood Geocoin\">CWG</abbr> for exchange? Then you are at the right place. You don't know what CWG is? Take a look at %s." =>
-        "Sbíráte <abbr title=\"Czech Wood Geocoin\">CWG</abbr>? Chcete mít ve své sbírce pořádek? Nabízíte <abbr title=\"Czech Wood Geocoin\">CWG</abbr> k výměně? Pak jste tu správně. Netušíte co je CWG? Pak se podívejte na %s.",
+        "Zbierate <abbr title=\"Czech Wood Geocoin\">xWG</abbr>? Chcete mať v zbierke poriadok? Ponúkate xWG a výmenu? Potom ste tu správne. Netušíte, Čo je to xWG? Pozrite sa na %s.",
     "To manage your collection, you must firs sign in. Without registration you can look at the %s and also at the %s of biggest collectors." =>
-        "Pro správu vlastní sbírky je vyžadováno přihlášení. Bez registrace se můžete podívat třeba na %s a také na %s nejlepších sběratelů.",
-    "xWG catalogue" => "katalog CWG",
-    "statistics" => "statistiky",
-    "Back to login" => "Zpět na přihlášení",
+        "Na spravovanie vlastnej zbierky sa vyžaduje prihlásenie. Bez registrácie sa môžete pozrieť napr. na %s a tiež na %s najlepších zberateľov.",
+    "xWG catalogue" => "katalóg xWG",
+    "statistics" => "štatistiky",
+    "Back to login" => "Späť na prihlásenie",
     "CWG Kolekce &copy; Niximor 2011-%s" => "CWG Kolekce &copy; Niximor 2011-%s",
     "<abbr title=\"Czech Wood Geocoin\">CWG</abbr> &copy; %s 2005-%s" => "<abbr title=\"Czech Wood Geocoin\">CWG</abbr> &copy; %s 2005-%s",
     "JPAgeo and Poník &amp; team" => "JPAgeo a Poník &amp; team",
     "CWG Catalogue &copy; %s" => "Katalog CWG &copy; %s",
-
     // Footer
     "CWG Collection is website for catalogization of your <abbr title=\"(Czech) Wood Geocoin\">xWG</abbr> collection. It is connected to the official catalogue and due to automatic synchronization it always contains up to date information about all the coins." =>
-        "CWG Kolekce je webová služba pro katalogizaci sbírky <abbr title=\"(Czech) Wood Geocoin\">xWG</abbr>. Je provázána s oficiálním katalogem a díky automatické synchronizaci obsahuje vždy nejaktuálnější data o všech kolečkách.",
+        "CWG Kolekce je webová služba na katalogizáciu zbierky xWG. Je previazaná s oficiálnym katalógom a vďaka automatickej synchronizácii obsahuje vždy najaktuálnejšie údaje o všetkých kolieskach.",
     "You can note various information about your collection (year, number of pieces, comment) and also any other information in the form of tags." =>
-        "Ke každému xWG můžete přidat podrobné informace týkající se vaší sbírky (ročník, počet kusů, poznámku) a zároveň libovolné další informace v podobě štítků (tagů).",
+        "Ku každému xWG môžete pridať podrobné informácie týkajúce sa vašej zbierky (ročník, počet kusov, poznámku) a zároveň ľubovoľné ďalšie informácie v podobe štítkov (tagov).",
     "You can offer xWG for exchange and easily have overview of your offered pieces such as offers from other users." =>
-        "xWG můžete také nabízet k výměně a snadno tak mít přehled o nabízených kusech jak svých, tak dalších uživatelů.",
+        "xWG môžete tiež ponúkať na výmenu a ľahko tak mať prehľad o ponúkaných kusoch svojich a od iných používateľov.",
     "Because of available API, it is possible to access the collection from another applications." =>
-        "Díky podpoře API je možno ke kolekci přistupovat z dalších aplikací.",
-    "Logged in as: %s" => "Přihlášen: %s",
-    "My public profile" => "Můj veřejný profil",
-    "My collection" => "Moje sbírka",
-    "My account" => "Můj účet",
-    "You are not logged in." => "Uživatel nepřihlášen.",
-
+        "Vďaka podpore API je možné k zbierke pristupovať z ďalších aplikácií.",
+    "Logged in as: %s" => "Prihlásený: %s",
+    "My public profile" => "Môj verejný profil",
+    "My collection" => "Moja zbierka",
+    "My account" => "Môj účet",
+    "You are not logged in." => "Používateľ neprihlásený.",
     // Page was not found
-    "Error 404: Page was not found" => "Chyba 404: Nenalezeno",
-    "Page, you are trying to open, does not exists." => "Stránka, kterou se pokoušíte otevřít, nebyla nalezena.",
-    "What are you looking for?" => "Co můžete hledat?",
-    "Specific xWG" => "Konkrétní xWG",
-    "User profile" => "Uživatelský profil",
-    "Something else" => "Něco jiného",
+    "Error 404: Page was not found" => "Chyba 404: Stránka sa nenašla",
+    "Page, you are trying to open, does not exists." => "Stránka, ktorú sa pokúšate otvoriť, sa nenašla.",
+    "What are you looking for?" => "Čo môžete hľadať?",
+    "Specific xWG" => "Konkrétne xWG",
+    "User profile" => "Používateľský profil",
+    "Something else" => "Niečo iné",
     "Link to this xWG is probably outdated and the xWG has been removed or did not existed. To find correct xWG, use %s." =>
-        "Odkaz na toto xWG je pravděpodobně zastaralý a dané xWG bylo odstraněno nebo nikdy neexistovalo. Pro nalezení správného xWG použijte %s.",
+        "Odkaz na toto xWG je pravdepodobne zastaraný a dané xWG bolo odstránené alebo nikdy neexistovalo. Na vyhľadanie správneho xWG použite %s.",
     "The user you are looking for is probably gone from the system. It is possible, that he didn't have account here, or that he renamed himself. You can try to find use using %s, which is accessible using the Search page or on the right top side of each user's profile." =>
-        "Hledaný uživatel se pravděpodobně v systému již nenachází. Je možné, že tu nikdy účet neměl, nebo že se přejmenoval. Můžete se pokusit nalézt uživatele pomocí %s, které je dostupné buď na vyhledávací stránce nebo v pravé části profilu každého z uživatelů.",
-    "user search" => "vyhledávání uživatelů",
+        "Hľadaný používateľ sa pravdepodobne v systéme už nenachádza. Je možné, že tu nikdy účet nemal, alebo že sa premenoval. Môžete sa pokúsiť nájsť používateľa pomocou %s, ktoré je dostupné buď na vyhľadávacej stránke alebo v pravej časti profilu každého z používateľov.",
+    "user search" => "vyhľadávania používateľov",
     "Link to this page is probably no longer valid. It is possible, that the structure of pages has changed or author of the link did a typo. In that case, try to select one of options from the top menu." =>
-        "Odkaz na tuto stránku již pravděpodobně není platný. Je možné, že se změnila struktura stránek, nebo se autor odkazu přepsal. V tom případě si vyberte některou z možností pokračování v horní nabídce stránky.",
+        "Odkaz na túto stránku už pravdepodobne nie je platný. Je možné, že sa zmenila štruktúra stránok, alebo sa autor odkazu prepísal. V tom prípade si vyberte niektorú z možností pokračovania v hornej ponuke stránky.",
     "It is also possible, that the page is available only after you log in. To log in, click %s." =>
-        "Je také možné, že se stránka nachází v části webu, která je přístupná pouze po přihlášení. Přihlásit se můžete %s.",
-    "here" => "zde",
+        "Je tiež možné, že sa stránka nachádza v časti webu, ktorá je prístupná len po prihlásení. Prihlásiť sa môžete %s.",
+    "here" => "tu",
     "If you believe this is an error, use form to %s." =>
-        "Pokud se domníváte, že se jedná o chybu, použijte formulář pro %s.",
-    "report problem" => "nahlášení problému",
+        "Ak sa domnievate, že ide o chybu, použite formulár na %s.",
+    "report problem" => "nahlásenie problému",
 );
