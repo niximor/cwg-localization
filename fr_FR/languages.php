a7bbc6867742afbf8924f288249dd7c4928e9862
Verturin <verturin@live.fr>
2016-10-17 14:01:02 +0200

    Create languages.php

---
+++ fr_FR/languages.php
@@ -0,0 +1,13 @@
+<?php
+
+// Language names
+
+$strings = array(
+    "Language switcher" => "Selectionner la langue",
+
+    "Czech" => "Česky",
+    "Slovak" => "Slovensky",
+    "English" => "Anglicky",
+    "France" => "Francais",
+    "Polish" => "Poľsky",
+);
