8351dfcdf2e34ab37ca4a74dae26c65ff01ba853
Verturin <verturin@live.fr>
2017-03-14 11:56:22 +0100

    Create account.php

---
+++ fr_FR/account.php
@@ -0,0 +1,31 @@
+<?php
+
+// Account related messages
+
+$strings = array(
+    "Sign in" => "Přihlášení",
+    "Recover lost password" => "Obnovit zapomenuté heslo",
+    "Sign up" => "Registrace",
+    "Create account" => "Vytvořit účet",
+    "Here you can create new account to access the site." => "Zde si můžete vytvořit uživatelský účet pro přístup k těmto stránkám.",
+    "User name" => "Uživatelské jméno",
+    "Enter your user name." => "Zadejte svoje uživatelské jméno",
+    "Password" => "Heslo",
+    "Retype password" => "Heslo znovu",
+    "Email" => "Email",
+    "Enter the email, that you have filled in during the registration or in the settings. You will receive new password to this email." =>
+        "Zadejte email, který jste zadali při registraci nebo později v nastavení. Na tento email obdržíte nové heslo.",
+    "Fields marked red are required." => "Červeně označená pole jsou povinná.",
+    "Finish registration" => "Dokončit registraci",
+    "Remember me" => "Zapamatovat přihlášení",
+    "Selected user name already exists." => "Zvolené uživatelské jméno již existuje.",
+    "Passwords did not match." => "Hesla se neshodují.",
+    "Registration has been completed successfully. You can now sign in." => "Registrace byla úspěšně dokončena. Nyní se můžete přihlásit.",
+    "Selected user name already exists." => "Zadané uživatelské jméno již existuje.",
+    "Password has been successfully changed. New password was emailed to you." =>
+        "Heslo bylo úspěšně změněno. Nové heslo bylo odesláno na email.",
+    "Specified user name / email combination was not found." =>
+        "Zadaná kombinace uživatelského jména a hesla nebyla nalezena.",
+    "Recover" => "Obnovit",
+    "Authentification failed. Invalid username or password." => "Přihlášení selhalo. Chybné uživatelské jméno nebo heslo.",
+);
