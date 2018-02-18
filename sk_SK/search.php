9bd8239b91c22a1aa625484e7034ab31c2735f6f
geoPetros <peter@miksik.sk>
2018-02-18 09:11:18 +0100

    Update search.php

--- sk_SK/search.php
+++ sk_SK/search.php
@@ -1,31 +1,45 @@
 <?php
+
 // Catalogue search page
+
 $strings = array(
     "Quick xWG collection" => "Rýchle zaradenie xWG",
     "Following values will be used for quick xWG collection using the Collect button in the search results." => "Nasledujúce údaje budú použité na rýchle zaradenie xWG do zbierky.",
+    
     "User search" => "Vyhľadávanie používateľov",
     "Search user" => "Vyhľadať používateľa",
     "Search" => "Vyhľadať",
     "Search!" => "Vyhľadať!",
     "Following users has been found to your search query '%s':" => "Na dopyt '%s' sa našli nasledujúci používatelia:",
     "Nothing was found for your query '%s'." => "Na dopyt '%s' sa nikto nenašiel.",
+    
     "Search result" => "Výsledky vyhľadávania",
     "Fill in the search query in the left side of the screen. You must enter at least three letters from the xWG name." =>
         "Vyplňte formulár na ľavej strane obrazovky. Pre funkciu vyhľadávania musíte vyplniť aspoň tri písmená z názvu xWG.",
     "Nothing was found for given search query. Try to enter only part of the name. If that does not help, try to hold until the xWG will be inserted into the catalogue." =>
-        "Na zadaný dopyt sa nič nenašlo. Skúste zadať len časť názvu. Ak ani to nepomôže, skúste vydržať, kým bude xWG do katalógu doplnené.",
+    "Na zadaný dopyt sa nič nenašlo. Skúste zadať len časť názvu. Ak ani to nepomôže, skúste vydržať, kým bude xWG do katalógu doplnené.",
+    
     "Collect" => "Zaradiť",
+    
     "Show me the results" => "Zobraziť výsledky",
+    
     // xWG indicators
     "xWG is not part of official catalogue" => "xWG nezaradené v oficiálnom katalógu",
     "You have at least one piece of this xWG in your collection." => "V zbierke máte aspoň jeden kus tohto xWG.",
     "You offer this xWG." => "Toto xWG ponúkate.",
     "You want this xWG." => "Toto xWG chcete.",
     "You don't want this xWG." => "Toto xWG nechcete.",
+    
     // xWG popup
     "Put into collection" => "Zaradiť do zbierky",
-    "Offer" => "Ponúknuť",
+    
+    "Offer" => "Ponúknuť",    
     "Offer for exchange" => "Ponúknuť na výmenu",
+    
     "Want" => "Chcem",
     "Don't want" => "Nechcem",
+
+    "Found: %d items" => "Nájdené položky: %d",
+    "&lt; Previous" => "&lt; Predch.",
+    "Next &gt;" => "Nasled. &gt;",
 );
