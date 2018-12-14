c85923134e14865547ca924a4cfd55bb9939d730
Michal Kuchta <niximor@gmail.com>
2018-12-14 15:17:13 +0100

    feat: Tags administration

--- cs_CZ/tags.php
+++ cs_CZ/tags.php
@@ -18,4 +18,35 @@ $strings = array(
     // Tags page in user's profile
     "Here you can find list of all your tags that you assigned to some xWGs. By clicking the tag, you can filter xWGs labeled with this tag." =>
         "Zde naleznete seznam štítků, které jste použili k označení xWG. Kliknutím na štítek vyfiltrujete xWG označená tímto štítkem.",
+
+    // Administration
+    "Tags management" => "Správa štítků",
+    "Modify tag %s: %s" => "Upravit štítek %s: %s",
+    "Create new %s tag" => "Vytvořit nový štítek typu %s",
+
+    "Given tag category was not found." => "Kategorie štítků nebyla nalezena.",
+    "Given tag was not found in specified category." => "Štítek nebyl nalezen ve zvolené kategorii.",
+
+    "Name" => "Název",
+    "Default name:" => "Výchozí jméno:",
+    "Name that will be used when there is no language-specific name." => "Jméno, které bude použito když neexistuje specifické jméno pro aktuální jazyk zvolený uživatelem.",
+    "Name (%s):" => "Jméno (%s):",
+    "This name already exists for different tag in this category." => "Tento název již existuje pro jiný štítek v této kategorii.",
+
+    "ISO code" => "ISO kód",
+    "Country ISO code:" => "ISO kód země:",
+    "At least one of ISO codes is already present for different tag in this category." => "Alespoň jeden ISO kód již existuje u jiného štítku v této kategorii.",
+    "This ISO code is already present for different tag in this category." => "Tento ISO kód již existuje u jiného štítku v této kategorii.",
+
+    "Create" => "Vytvořit",
+    "Back" => "Zpět",
+
+    "Tag %s has been successfully created." => "Štítek %s byl úspěšně vytvořen.",
+    "Tag %s has been successfully modified." => "Štítek %s byl úspěšně upraven.",
+    "Tag %s has been successfully removed." => "Štítek %s byl úspěšně odstraněn.",
+    "Cannot remove tag, it is used by at least one xWG." => "Štítek nelze odstranit. Je použit u alespoň jednoho xWG.",
+
+
+
+
 );
