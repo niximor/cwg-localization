7884e403994d6f36924ccc1c6704d92ec4965c88
geoPetros <peter@miksik.sk>
2018-12-19 15:41:55 +0100

    Update tags.php
    Added new lines

--- sk_SK/tags.php
+++ sk_SK/tags.php
@@ -18,4 +18,35 @@ $strings = array(
     // Tags page in user's profile
     "Here you can find list of all your tags that you assigned to some xWGs. By clicking the tag, you can filter xWGs labeled with this tag." =>
         "Tu nájdete zoznam štítkov, ktoré ste použili na označenie xWG. Kliknutím na štítok vyfiltrujete xWG označené týmto štítkom.",
+
+    // Administration
+    "Tags management" => "Správa štítkov",
+    "Modify tag %s: %s" => "Upraviť štítok %s: %s",
+    "Create new %s tag" => "Vytvoriť nový štítok typu %s",
+
+    "Given tag category was not found." => "Kategória štítkov sa nenašla.",
+    "Given tag was not found in specified category." => "Štítok sa vo vybranej kategórii nenašiel.",
+
+    "Name" => "Názov",
+    "Default name:" => "Predvolený názov:",
+    "Name that will be used when there is no language-specific name." => "Názov, ktorý bude použitý, ak neexistuje špecifický názov pre aktuálny jazyk zvolený používateľom.",
+    "Name (%s):" => "Názov (%s):",
+    "This name already exists for different tag in this category." => "Tento názov už existuje pre iný štítok v tejto kategórii.",
+
+    "ISO code" => "ISO kód",
+    "Country ISO code:" => "ISO kód krajiny:",
+    "At least one of ISO codes is already present for different tag in this category." => "Aspoň jeden ISO kód už existuje pri inom štítku v tejto kategórii.",
+    "This ISO code is already present for different tag in this category." => "Tento ISO kód už existuje pri inom štítku v tejto kategórii.",
+
+    "Create" => "Vytvoriť",
+    "Back" => "Späť",
+
+    "Tag %s has been successfully created." => "Štítok %s bol úspešne vytvorený.",
+    "Tag %s has been successfully modified." => "Štítok %s bol úspešne upravený.",
+    "Tag %s has been successfully removed." => "Štítok %s bol úspešne odstránený.",
+    "Cannot remove tag, it is used by at least one xWG." => "Štítok nie je možné odstrániť. Je použitý aspoň pri jednom xWG.",    
+    
+
+
+
 );
