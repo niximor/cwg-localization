bec1617e4b9d4e38c59571d57c013b6f337bb742
geoPetros <peter@miksik.sk>
2018-12-19 16:09:52 +0100

    Update cwg_ops.php
    Added lines

--- sk_SK/cwg_ops.php
+++ sk_SK/cwg_ops.php
@@ -1,38 +1,55 @@
 <?php
+
 // New xWG form and related messages
+
 $strings = array(
     "Insert new xWG" => "Vložiť nové xWG",
     "This page can be used to enter xWG, that isn't (so far) present in the official catalogue. Please, take extreme caution of what you are entering. Mainly, please, pay attention to <strong>xWG NAME</strong>, which should contain exactly what is written <strong>AROUND THE xWG</strong>, not inside. Before you submit new xWG, please check, that xWG is not entered with some similar name (for example including \"&amp; team\", \"&amp; family\" and similar). You will spare administrator's time who will need to manually fix your error when it is found. When you enter incorrect xWG multiple times, it may lead to revocation of your privilege to insert new items." =>
         "Slúži na zadanie xWG, ktoré (zatiaľ) nie je umiestnené v katalógu. Pri zadávaní si dajte pozor, čo zadávate. Najmä <strong>NÁZOV xWG</strong>, ktorý by mal presne zodpovedať tomu, čo je napísané <strong>PO OBVODE xWG</strong>, nie vo vnútri. Pred zadaním tiež skontrolujte, či už nie je xWG zadané s nejakým podobným názvom (napr. \"&amp; team\", \"&amp; family\" apod.). Ušetríte tak prácu aj nervy administrátorom v dobe, kedy niekto na chybu príde, a administrátori ju potom budú musieť ručne riešiť. Opakované chybné zadávanie xWG môže byť potrestané až zákazom vkladania ďalších záznamov.",
+ 
     "Describe the picture on the xWG into the <strong>Note</strong> field." =>
         "Do položky <strong>Poznámka</strong> napíšte, aký je na xWG motív, prípadne nejaký iný spôsob, ako toto xWG identifikovať.",
+    
     "xWG information" => "Údaje o xWG",
     "Enter what is written <strong>AROUND</strong> xWG." =>
         "Vpíšte to, čo je <strong>PO OBVODE</strong> xWG.",
     "Write by words, what image is burned on the xWG." =>
         "Popíšte slovami obrázkový motív, ktorý je na xWG vypálený.",
-        "Photo" => "Obrázok",
+    
+    "Photo" => "Obrázok",
+    "Photo:" => "Obrázok:",
+    
     "Please, provide photo of xWG that you want to enter. The photo does not have to be in perfect quality, it won't be shown on the xWG listing until approved by admin. It is used only to check information entered here." =>
         "Priložte fotografiu xWG, ktoré chcete vložiť. Fotografia nemusí byť kvalitná, nebude sa štandardne zobrazovať ako obrázok xWG, dokým ju admin neupraví. Fotografia sa používa na overenie alebo úpravu údajov zadaných v tomto formulári.",
+    
     "Backside" => "Zadná strana",
     "If xWG has nonstandard backside, please, upload the backside too." => "Ak má xWG neštandardnú zadnú stranu, nahrajte aj jej obrázok.",
+    
+    "The file is not valid image. Only JPEG file format is supported." => "Obrázok nemá správny formát. Musí byť vo formáte JPEG.",
+    "Image must have at least 300x300px." => "Obrázok musí mať najmenej 300x300px.",
+    
     "Insert" => "Vložiť",
+    
     "Another xWG with same name and version already exists in this catalogue." =>
         "xWG s týmto názvom a verziou v katalógu už existuje.",
     "xWG has been successfully placed into the catalogue." =>
-        "xWG bolo úspešne vložené do katalógu.",
+    "xWG bolo úspešne vložené do katalógu.",
+    
     "xWGs entered by me" => "Mnou zadané xWG",
     "Users<sup>*</sup>" => "Používateľov<sup>*</sup>",
+    
     "Modify" => "Upraviť",
     "Modify xWG %s (version %s)" => "Upraviť xWG %s (verzia %s)",
     "Modify xWG %s" => "Upraviť xWG %s",
     "xWG has been successfully modified." => "xWG bolo úspešne upravené.",
     "You have insufficient privileges." => "Nemáte dostatočné oprávnenia.",
+    
     "Remove" => "Odstrániť",
     "Do you really want to remove this xWG? Operation cannot be undone." => "Naozaj chcete toto xWG odstrániť? Úkon sa nedá vrátiť!",
     "xWG has been successfully removed." => "xWG bolo úspešne odstránené.",
     "xWG cannot be removed, because it is collected by at least one user." =>
         "xWG nie je možné odstrániť, ak ho má v zbierke aspoň jeden používateľ.",
+    
     "Join to..." => "Zlúčiť s...",
     "Join xWG %s (version %s)" => "Zlúčiť xWG %s (verzia %s)",
     "Join xWG %s" => "Zlúčiť xWG %s",
@@ -50,7 +67,20 @@ $strings = array(
     "xWG&nbsp;%s, which you don't want, has been joined to xWG&nbsp;%s. Please, check whether this is the piece that you don't want." =>
         "xWG&nbsp;%s, ktoré nechcete, bolo zlúčené s xWG&nbsp;%s. Skontrolujte, či ide stále o kúsok, ktorý nechcete.",
     "Join has been performed by %s." => "Zlúčenie vykonal/a %s.",
+    
     "Picture" => "Obrázok",
     "* Number of users, which has this xWG in their collection. xWG can be removed only when noone has it in the collection." =>
         "* Počet používateľov, ktorí majú toto xWG vo svojej zbierke. xWG sa dá odstrániť len vtedy, ak ho nikto nemá v zbierke.",
+    
+    "* Number of users, which has this xWG in their collection. xWG can be removed only when noone has it in the collection." =>
+        "* Počet používateľov, ktorí majú toto xWG vo svojej zbierke. xWG možno vymazať len vtedy, ak ho nikto nemá v zbierke.",
+    
+    "Show archived" => "Zobraziť archivované",
+    "Hide archived" => "Skryť archivované",
+    
+    "%d (%s pieces)" => "%d (%s ks)",
+    
+    "Communication" => "Komunikácia",
+    "Message can be seen by any administrator, but noone else. All messages are hidden when xWG gets published. They only remain in your profile." => "Správy vidia všetci administrátori, ale nikto iný. Všetky správy sú skryté v momente uverejnenia xWG. Sú potom dostupné len vo vašom profile.",
+    "If you want to attach another image for the xWG, you can do so here. Again, image must be in JPEG format with minimum dimensions of 300x300px." => "Ak chcete pripojiť ďalší obrázok pre toto xWG, môžete tak urobiť tu. Stále platia rovnaké pravidlá – musí byť vo formáte JPEG s rozmermi minimálne 300x300px.",
 );
